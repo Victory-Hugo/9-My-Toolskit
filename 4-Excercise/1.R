2ffdf2566168ac001e562f6122cada6249fed84d
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-07-14 09:14:13 +0800

    2025年7月14日

--- 4-Excercise/1.R
+++ 4-Excercise/1.R
@@ -13,3 +13,25 @@ flights |> group_by(year, month, day) |>
    summarise(
     mean = mean(dep_delay, na.rm = TRUE)
    ) 
+
+
+setwd('/mnt/c/Users/Administrator/Desktop')
+library(ggsci)
+diamonds |> 
+   filter(carat < 3 ) |>
+   ggplot(
+      aes(x = carat,color = cut)
+   ) +
+   geom_freqpoly(binwidth = 0.1) +
+   scale_color_aaas() +
+   coord_cartesian(
+      xlim = c(0,1) #!重点
+   )
+
+diamonds |> 
+    ggplot(
+      aes(x = x ,y =y )
+    ) +
+    geom_point(aes(color = "#1E1F1C")) +
+    coord_cartesian(ylim = c(2.5,10),
+                    xlim = c(3,10)) 
